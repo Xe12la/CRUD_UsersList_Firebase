5e349332f48d58537bb790da617e330f7264f035
Xe12la <aumanalexander3@gmail.com>
2023-05-12 20:49:28 +0800

    ..

---
+++ database/firebaseDb.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyC5b95OWWQvqqfkVivoaPrGDA40Y2mi8jg",
+    authDomain: "todo-app-21437.firebaseapp.com",
+    projectId: "todo-app-21437",
+    storageBucket: "todo-app-21437.appspot.com",
+    messagingSenderId: "978546812938",
+    appId: "1:978546812938:web:0ecf66c9f84a7815d9d7f3"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+export default firebase;
